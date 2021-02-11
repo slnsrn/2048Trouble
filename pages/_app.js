3beff5a68056072154600662f3f4c08da3b45fbe
Selin Serin <selin.serin@frontiercargroup.com>
2021-02-11 16:28:41 +0100

    update seo data

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Head from 'next/head'
 
 import '../styles/globals.css'
@@ -7,6 +8,18 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>2048Trouble</title>
+        <meta name="og:type" content="website" />
+        <meta name="description" content="Play 2048 with a troubled start" />
+        <meta name="og:title" content="2048Trouble" />
+        <meta name="keywords" content="game, 2048" />
+        <meta name="og:url" content="https://2048Trouble.com" />
+        <meta name="og:description" content="Play 2048 with a troubled start" />
+        <meta name="og:image" content="https://2048Trouble.com/app-image.png" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="2048Trouble" />
+        <meta name="twitter:site" content="@slnsrn" />
+        <meta name="twitter:description" content="Play 2048 with a troubled start" />
+        <meta name="twitter:image" content="https://2048Trouble.com/app-image.png" />
         <script
           data-ad-client="ca-pub-2322848954109478"
           async
